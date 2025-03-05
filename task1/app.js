a436857692fcdc91fb3bf0b2876605f5121f6674
yaqutfettah0 <yaqutfettah@gmail.com>
2025-03-05 14:08:19 +0400

    new file

--- task1/app.js
+++ task1/app.js
@@ -1,21 +1,7 @@
-const hefteninGunu =  prompt("heftenin necenci gunudur?(1-7)");
-    let gun = "";
+function name (ad) {
+    console.log(`<${ad} is active now>`);
+}
 
-    if (hefteninGunu==1)  
-        alert("Bu gun Bazar ertesidir.");
-    else if (hefteninGunu==2) 
-        alert ("Bu gun Cerbenbe axsamidir");
-    else if (hefteninGunu==3) 
-        alert ("Bu gun Cersenbedir.");
-    else if (hefteninGunu==4) 
-        alert ("Bu gun Cume axsamidir.");
-    else if (hefteninGunu==5) 
-        alert ("Bu gun Cumedir.");
-    else if (hefteninGunu==6) 
-        alert ("Bu gun Senbedir.");
-    else if (hefteninGunu==7) 
-        alert ("Bu gun Bazardir.");
-    else 
-        alert ("Ay bala 1-den 7-ye qeder reqem daxil et.")
-    
-console.log(gun);
+name("Emma");
+name("Maria");
+name("Kate");
